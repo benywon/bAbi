55329b7867c745b969a5899ae1b888829e4b33ca
benywon <bingning.wang@nlpr.ia.ac.cn>
2016-03-04 17:27:44 +0800

    1

---
+++ QASentForServer.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+from dataPreprocess import dataPreprocess
+
+
+__author__ = 'benywon'
+
+
+class QASentForServerdataPreprocess(dataPreprocess):
+    def __init__(self,
+                 use_clean=False,
+                 **kwargs):
+        dataPreprocess.__init__(self, **kwargs)
+        self.use_clean = use_clean
+        self.path = './data/QAsent/'
+        append_str = '_batch' if self.batch_training else ''
+        append_str += '_clean' if self.use_clean else ''
+        self.data_pickle_path = self.path + 'QAsent' + append_str + '.pickle'
+        if self.reload:
+            self.build_data_set()
+        else:
+            self.load_data()
+        self.calc_data_stat()
+        self.dataset_name = 'QASent'
